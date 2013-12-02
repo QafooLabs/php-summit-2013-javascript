7c9ced44579d65b8c55d87d5cfc611374c7c3dde
Jakob Westhoff <jakob@westhoffswelt.de>
2013-12-02 14:49:34 +0100

    Finished roomplan rendering

--- jquery/htdocs/js/app.js
+++ jquery/htdocs/js/app.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-    $("[data-role='roomplan']").roomplan();
+    // Initialize all the handlebar templates
+    var Template = {};
+    Template.roomplan = Handlebars.compile($("#template-roomplan").html());
+
+    // Register all used template partials (subtemplates)
+    Handlebars.registerPartial("row", $("#template-row").html());
+    Handlebars.registerPartial("seat", $("#template-seat").html());
+
+    // Register Handlebars helpers for proper if checks
+    Handlebars.registerHelper('if-eq', function(a, b, options) {
+        if (a === b) {
+            return options.fn(this);
+        }
+        else {
+            return options.inverse(this);
+        }
+    });
+
+    var roomplanRequest = $.getJSON("data/roomplan.json");
+
+    $.when(roomplanRequest).done(function(roomplanData) {
+        $("[data-role='roomplan']").roomplan({
+            template: Template.roomplan,
+            data: roomplanData
+        });
+    });
 });
